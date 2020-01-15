106d254276d535abe4d256dfbcae7437d14d955c
chris <416734061@qq.com>
2020-01-15 21:02:40 +0800

    add Client/Server communication

--- src/main/java/nio/testBlock/Client.java
+++ src/main/java/nio/testBlock/Client.java
@@ -10,42 +10,53 @@ import java.nio.file.StandardOpenOption;
 
 /**
  * 用阻塞NIO完成网络通信的客户端
+ * 客户端需要收到服务端接收成功的反馈
  */
 public class Client {
+    public static void main(String[] args) {
 
-//    public static void main(String[] args) {
-//        SocketChannel socketChannel = null;
-//        FileChannel fileChannel = null;
-//        try {
-//            //1.获取通道
-//            socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 9898));
-//            fileChannel = FileChannel.open(Paths.get("1.jpg"), StandardOpenOption.READ);
-//            //2.分配缓存区
-//            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-//            //3.读取本地文件并发送到服务器
-//            while (fileChannel.read(byteBuffer) != -1){
-//                byteBuffer.flip();
-//                socketChannel.write(byteBuffer);
-//                byteBuffer.clear();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }finally {
-//            //4.关闭资源
-//            if (null != fileChannel){
-//                try {
-//                    fileChannel.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if(null != socketChannel){
-//                try {
-//                    socketChannel.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//    }
+        SocketChannel socketChannel = null;
+        FileChannel fileChannel = null;
+        try {
+            //1.获取通道(socketChannel + FileChannel)
+            socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 9898));
+            fileChannel = FileChannel.open(Paths.get("1.jpg"), StandardOpenOption.READ);
+            //2.分配缓存区
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            //3.读取本地文件并发送到服务器
+            while (fileChannel.read(byteBuffer) != -1){
+                byteBuffer.flip();
+                socketChannel.write(byteBuffer);
+                byteBuffer.clear();
+            }
+            socketChannel.shutdownOutput(); //告诉服务端已经发送数据完成
+
+            //4.接收服务端的反馈
+            int len;
+            while ((len=socketChannel.read(byteBuffer))!= -1){
+                byteBuffer.flip();
+                System.out.println(new String(byteBuffer.array(),0,len));
+                byteBuffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //5.关闭资源
+            if (null != fileChannel){
+                try {
+                    fileChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != socketChannel){
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
--- src/main/java/nio/testBlock/Server.java
+++ src/main/java/nio/testBlock/Server.java
@@ -11,54 +11,59 @@ import java.nio.file.StandardOpenOption;
 
 /**
  * 用阻塞NIO完成网络通信的服务端
+ * 服务端接收成功后需要反馈给客户端
  */
 public class Server {
 
-//    public static void main(String[] args) {
-//        ServerSocketChannel serverSocketChannel = null;
-//        FileChannel outFileChannel = null;
-//        SocketChannel socketChannel = null;
-//        try {
-//            //1.获取通道
-//            serverSocketChannel = ServerSocketChannel.open();
-//            outFileChannel = FileChannel.open(Paths.get("2.jpg"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
-//            //2.绑定连接
-//            serverSocketChannel.bind(new InetSocketAddress(9898));
-//            //3.获取客户端连接的通道
-//            socketChannel = serverSocketChannel.accept();
-//            //4.分配缓冲区
-//            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-//            //5.接受客户端的数据，并保存到本地
-//            while (socketChannel.read(byteBuffer) != -1){
-//                byteBuffer.flip();
-//                outFileChannel.write(byteBuffer);
-//                byteBuffer.clear();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            //6.关闭资源
-//            if (null != socketChannel){
-//                try {
-//                    socketChannel.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (null != outFileChannel){
-//                try {
-//                    outFileChannel.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if(null != serverSocketChannel){
-//                try {
-//                    serverSocketChannel.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//    }
+    public static void main(String[] args) {
+        ServerSocketChannel serverSocketChannel = null;
+        FileChannel fileChannel = null;
+        SocketChannel socketChannel = null;
+        try {
+            //1.获取通道(serverSocketChannel + FileChannel)
+            serverSocketChannel = ServerSocketChannel.open();
+            fileChannel = FileChannel.open(Paths.get("2.jpg"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+            //2.绑定连接
+            serverSocketChannel.bind(new InetSocketAddress(9898));
+            //3.获取客户端连接通道
+            socketChannel = serverSocketChannel.accept();
+            //4.分配缓存区
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            //5.接收客户端的数据并保存到本地
+            while (socketChannel.read(byteBuffer) != -1){
+                byteBuffer.flip();
+                fileChannel.write(byteBuffer);
+                byteBuffer.clear();
+            }
+            //6.反馈给客户端
+            byteBuffer.put("服务端接收到了。。".getBytes());
+            byteBuffer.flip();
+            socketChannel.write(byteBuffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //7.关闭资源
+            if(null != socketChannel){
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != fileChannel){
+                try {
+                    fileChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != serverSocketChannel){
+                try {
+                    serverSocketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
