488beeb18c380051462260af431a5bdb76841b03
ChrisQian <416734061@qq.com>
2020-01-26 18:03:12 +0800

    netty handlerChain invocation

---
+++ src/main/java/netty/handlerChain/MyServer.java
@@ -0,0 +1,32 @@
+package netty.handlerChain;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+    public static void main(String[] args){
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new MyServerInitializer()); //自定义一个初始化类
+
+        try {
+            ChannelFuture channelFuture = serverBootstrap.bind(9898).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
---
+++ src/main/java/netty/handlerChain/MyServerInitializer.java
@@ -0,0 +1,21 @@
+package netty.handlerChain;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        System.out.println("------initChannel() invoked-------");
+        ChannelPipeline pipeline = ch.pipeline();
+        //入站的handler进行解码 MyByteToLongDecoder
+        pipeline.addLast(new MyByteToLongDecoder());
+        //出站的handler进行编码
+        pipeline.addLast(new MyLongToByteEncoder());
+        //自定义的handler 处理业务逻辑
+        pipeline.addLast(new MyServerHandler());
+    }
+}
