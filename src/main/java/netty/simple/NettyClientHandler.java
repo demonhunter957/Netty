18fbdc7b9c4b69c82f8d0aebe94f10adba1d1b31
ChrisQian <416734061@qq.com>
2020-01-26 18:49:26 +0800

    minor change

--- src/main/java/netty/simple/NettyClientHandler.java
+++ src/main/java/netty/simple/NettyClientHandler.java
@@ -8,19 +8,33 @@ import io.netty.util.CharsetUtil;
 
 public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) {
+        System.out.println("-----------------channelRegistered() starts");
+        System.out.println(ctx.channel().hashCode());
+        System.out.println("-----------------channelRegistered() ends");
+    }
+
     //当通道就绪的时候触发的方法
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("服务端中ctx的类型：" + ctx.channel().getClass());
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println("---------------channelActive() starts---------------" + System.currentTimeMillis());
+        System.out.println(ctx.channel().hashCode());
+        System.out.println("channel的类型：" + ctx.channel().getClass());
+        //向通道中发送数据
         ctx.writeAndFlush(Unpooled.copiedBuffer("hello server...", CharsetUtil.UTF_8));
+        System.out.println("---------------channelActive() ends---------------");
     }
 
     //当通道有读取事件时调用的方法
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg){
+        System.out.println("-----------------------channelRead() starts-----------");
+        System.out.println(ctx.channel().hashCode());
         ByteBuf byteBuf = (ByteBuf)msg;
         System.out.println("服务器回复的消息是： " + byteBuf.toString(CharsetUtil.UTF_8));
         System.out.println("服务器的地址是： " + ctx.channel().remoteAddress());
+        System.out.println("-----------------------channelRead() ends-----------");
     }
 
     @Override
--- src/main/java/netty/simple/NettyServerHandler.java
+++ src/main/java/netty/simple/NettyServerHandler.java
@@ -16,8 +16,9 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     msg:服务端发来的消息
      */
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
         System.out.println("--------------channelRead() starts-----------------");
+        System.out.println(ctx.channel().hashCode());
         System.out.println("ctx的类型： " + ctx.getClass()); //io.netty.channel.DefaultChannelHandlerContext
         Channel channel = ctx.channel();
         ChannelPipeline pipeline = ctx.pipeline(); //pipeline本质是个双向链表，负责出栈入栈。
@@ -29,17 +30,17 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 
     //读取客户端消息完毕后调用的方法
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+    public void channelReadComplete(ChannelHandlerContext ctx) {
         System.out.println("---------------channelReadComplete() starts------------");
+        System.out.println(ctx.channel().hashCode());
         //给客户端反馈消息
         ctx.writeAndFlush(Unpooled.copiedBuffer("hello 客户端！", CharsetUtil.UTF_8));
         System.out.println("---------------channelReadComplete() ends------------");
     }
 
-    //异常发生时调用的方法
-    //一般处理异常的方法是关闭通道
+    //异常发生时调用的方法，一般处理异常的方法是关闭通道
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
     }
@@ -47,13 +48,15 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRegistered(ChannelHandlerContext ctx)  {
         System.out.println("--------channelRegistered() starts------------");
+        System.out.println(ctx.channel().hashCode());
         System.out.println(ctx.channel().getClass()); //io.netty.channel.socket.nio.NioSocketChannel
         System.out.println("--------channelRegistered() ends------------");
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        System.out.println("---------channelActive() starts------------");
+        System.out.println("---------channelActive() starts------------" + System.currentTimeMillis());
+        System.out.println(ctx.channel().hashCode());
         System.out.println(ctx.channel().getClass()); //io.netty.channel.socket.nio.NioSocketChannel
         System.out.println("---------channelActive() ends------------");
     }
